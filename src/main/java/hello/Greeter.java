c3f5e1a27de7375106279e8e7e69060d32148d10
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-12 10:59:32 +0900

    feat. 예제

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,14 @@
+package main.java.hello;
+
+/**
+ * <pre>
+ * User: clean_brain
+ * Date: 2023-11-12
+ * Comments: 
+ * </pre>
+ */
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package main.java.hello;
+
+/**
+ * <pre>
+ * User: clean_brain
+ * Date: 2023-11-12
+ * Comments: 
+ * </pre>
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
