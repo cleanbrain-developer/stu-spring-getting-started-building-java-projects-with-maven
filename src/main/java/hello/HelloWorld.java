1630bbe53e30b376db7c64ee327549038b82a931
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-12 11:15:24 +0900

    feat. 예제

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,5 +1,7 @@
 package main.java.hello;
 
+import org.joda.time.LocalTime;
+
 /**
  * <pre>
  * User: clean_brain
@@ -9,6 +11,8 @@ package main.java.hello;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
